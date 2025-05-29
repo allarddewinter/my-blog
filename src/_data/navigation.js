7c6f60cab9e88c11d8714d0a0b373203bf8df705
allarddewinter <allard070@gmail.com>
2025-05-29 10:29:21 +0200

    Update navigation.js

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -11,8 +11,8 @@ export default {
   ],
   bottom: [
     {
-      text: 'Style guide',
-      url: '/styleguide/'
+      text: 'Sustainability',
+      url: '/sustainability/'
     },
     {
       text: 'ToDo',
